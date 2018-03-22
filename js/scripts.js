bd7558c38b79d966bdfc32df9dac03ecb3e0e9c2
Joy Paglinawan and Susan Southall <joy@>
2018-03-22 15:28:44 -0700

    Add some branching.

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+    var ninjaInput = $("input#job1").val();
+    var pirateInput = $("input#job2").val();
+    var alienInput = $("input#job3").val();
+    var yourJob;
+
+      if (ninjaInput===true && pirateInput===false && alienInput===false) {
+        alert("You are a ninja!!");
+        yourJob="Ninja";
+      }else if (pirateInput===true && ninjaInput===false && alienInput===false) {
+        alert("You are a pirate!!");
+        yourJob="Pirate";
+      }else if (alienInput===true && ninjaInput===false && pirateInput===false) {
+        alert("You are an alien!!");
+        yourJob="Alien";
+      }else{
+        alert("Only one of these can be true.");
+      }
+
+      $("#what-are-you").text(yourJob);
+      // $(".pirate-job").text(pirateInput);
+      // $(".alien-job").text(alienInput);
+
+
+
+      event.preventDefault();
+    });
+  });
+
+    //
+    // $(".ninja-job").text(ninjaInput);
+    // $(".pirate-job").text(pirateInput);
+    // $(".alien-job").text(alieanInput);
+    //
+
+    //
+    // $("#story").show();
+    //
+    // if $(userInput >= 3) {
+    //   alert("you are a ninja");
+    // }else if {
+    //   (userInput >=)
+    // }
+//
+//     event.preventDefault();
+//   })
+//
+// });

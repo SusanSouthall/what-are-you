059c6a932df8b84884b84f33a73a9ff5c8369326
Joy Paglinawan and Susan Southall <joy@>
2018-03-22 16:39:13 -0700

    add change to match booleans instead of strings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event){
-    var ninjaInput = $("input#job1").val();
-    var pirateInput = $("input#job2").val();
-    var alienInput = $("input#job3").val();
-    var yourJob;
+    var ninjaInput = $("input#ninja").val();
+    var pirateInput = $("input#pirate").val();
+    var alienInput = $("input#alien").val();
+    var yourJob = "";
 
-      if (ninjaInput===true && pirateInput===false && alienInput===false) {
+      if (ninjaInput==="true" && pirateInput==="false" && alienInput==="false") {
         alert("You are a ninja!!");
         yourJob="Ninja";
-      }else if (pirateInput===true && ninjaInput===false && alienInput===false) {
+      } else if (pirateInput==="true" && ninjaInput==="false" && alienInput==="false") {
         alert("You are a pirate!!");
         yourJob="Pirate";
-      }else if (alienInput===true && ninjaInput===false && pirateInput===false) {
+      } else if (alienInput==="true" && ninjaInput==="false" && pirateInput==="false") {
         alert("You are an alien!!");
         yourJob="Alien";
-      }else{
+      } else {
         alert("Only one of these can be true.");
       }
 
